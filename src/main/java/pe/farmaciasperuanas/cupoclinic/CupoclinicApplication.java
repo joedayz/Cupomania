51867c244f3a2e42188eaf19e6d69dd5f7da9ee4
Jose Amadeo Diaz Diaz <joseamadeodiazdiaz@MacBook-Pro-de-soporte.local>
2021-02-05 19:34:17 -0500

    cupomania

---
+++ src/main/java/pe/farmaciasperuanas/cupoclinic/CupoclinicApplication.java
@@ -0,0 +1,13 @@
+package pe.farmaciasperuanas.cupoclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CupoclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CupoclinicApplication.class, args);
+	}
+
+}
